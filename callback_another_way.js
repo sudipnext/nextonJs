259ea942d3f73cc65e5245ac729e2f2024bb5e27
sudipnext <parajulisudip123@outlook.com>
2022-01-13 14:46:13 +0545

    callback

--- callback_another_way.js
+++ callback_another_way.js
@@ -3,3 +3,4 @@ globalThis.setTimeout(function(){
 },5000);
 
 //another way to callback the function
+globalThis.setTimeout(()=> console.log("it has been 5 secs"), 5000);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,11 +1,3 @@
-
-// function soundMaker(sound, times){
-//     //closure function is makeSound
-//     function makeSound(){
-//         console.log(`${sound}`);
-//     }
-//     for(let i=0; i< times; i++){
-//         makeSound();
-
-//     }
-// }
+globalThis.setTimeout(function(){
+//     console.log(`It has been 5 secs`);
+// },5000);
