bca312da3346b1ab36592ef586951727050b46b6
sudipnext <parajulisudip123@outlook.com>
2022-01-13 14:33:29 +0545

    description

--- oop.js
+++ oop.js
@@ -17,4 +17,10 @@ then it comes to the curry.__proto__ and
 looks for NBAPlayer.prototype and find out the defin
 ation of the sayhello function
 
+
+we can also define throught the function directly on 
+the NBAPlayer but this will create again wehen we 
+call for every single player. so following the 
+programming rules we don't do that here following 
+the DRY= do not repeat
 */
