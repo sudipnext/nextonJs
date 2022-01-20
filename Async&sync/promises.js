937c39d355160a4389226923e013423ea09205ad
sudipnext <parajulisudip123@outlook.com>
2022-01-20 14:10:44 +0545

    relationship between promise and promise cycle

--- Async&sync/promises.js
+++ Async&sync/promises.js
@@ -7,12 +7,17 @@ let stocks = {
 
 let is_shop_open = true;
 
-let order =(time, work)=>{
-    return new Promise( (resolve, reject)=>{
-        if(is_shop_open){
-            resolve( work());
-        }else{
+let order = (time, work) => {
+    return new Promise((resolve, reject) => {
+        if (is_shop_open) {
+            setTimeout(() => {
+                resolve(work());
+
+            }, time)
+        } else {
             reject(console.log("Our shop is closed!"));
         }
     })
 }
+
+order(2000, ()=>console.log(`${stocks.Fruits[0]} was selected!`));
