c8b532e4518a6e98a5e17eb723689f9d85e2e835
sudipnext <parajulisudip123@outlook.com>
2022-01-20 06:48:52 +0545

    timeout added with defination

--- Async/async&sync.js
+++ Async/async&sync.js
@@ -16,3 +16,22 @@
 //asynchronous
 //==>This is the system where the code runs simultaneously
 //so called asynchronous system.
+
+//INTRO TO TIMEOUT
+//settimeout gets two parameters first the function and the another one
+//as time it's a asynchronous function.
+
+
+console.log("I am a music man")
+setTimeout(()=>{
+    console.log("pi pi piano")
+}, 5000)
+console.log("I can play the piano")
+
+
+console.log("Hello")
+
+console.log("boss! are you listening?")
+
+//in the result as we can se the timeout waits for 5secs before
+//actually executing that's called asynchronous.
