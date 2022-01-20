e44bd25113b07c64766f83c606eac1258f0be597
sudipnext <parajulisudip123@outlook.com>
2022-01-20 14:06:49 +0545

    .

--- Async&sync/promises.js
+++ Async&sync/promises.js
@@ -4,3 +4,15 @@ let stocks = {
     holder: ["cone", "cup", "stick"],
     toppings: ["chocolate", "peanuts"],
 };
+
+let is_shop_open = true;
+
+let order =(time, work)=>{
+    return new Promise( (resolve, reject)=>{
+        if(is_shop_open){
+            resolve( work());
+        }else{
+            reject(console.log("Our shop is closed!"));
+        }
+    })
+}
