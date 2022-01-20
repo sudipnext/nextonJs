a9e6d69c663eb5064c24d30571ae61556a9e06df
sudipnext <parajulisudip123@outlook.com>
2022-01-20 14:03:48 +0545

    an alternate to callbacks :)

---
+++ Async&sync/promises.js
@@ -0,0 +1,6 @@
+let stocks = {
+    Fruits: ["strawberry", "grapes", "banana", "apple"],
+    liquid: ["water", "ice"],
+    holder: ["cone", "cup", "stick"],
+    toppings: ["chocolate", "peanuts"],
+};
