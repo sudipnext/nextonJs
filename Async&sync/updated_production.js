306eacdfc9bce8a7a12f206115c0cd15b6db0136
sudipnext <parajulisudip123@outlook.com>
2022-01-20 13:56:06 +0545

    icecream making app

--- Async&sync/updated_production.js
+++ Async&sync/updated_production.js
@@ -1,36 +1,50 @@
+const { set } = require("express/lib/application");
+
 let stocks = {
-    Fruits : ["strawberry", "grapes", "banana", "apple"],
-    liquid : ["water", "ice"],
-    holder : ["cone", "cup", "stick"],
-    toppings : ["chocolate", "peanuts"],
- };
+    Fruits: ["strawberry", "grapes", "banana", "apple"],
+    liquid: ["water", "ice"],
+    holder: ["cone", "cup", "stick"],
+    toppings: ["chocolate", "peanuts"],
+};
 
- let order = (fruit_name, call_production) =>{
+let order = (fruit_name, call_production) => {
 
-    setTimeout(function(){
-  
-      console.log(`${stocks.Fruits[fruit_name]} was selected`)
-  
-  // Order placed. Call production to start
-  call_production();
-    
-    },2000)
-  };
-  
-  // 2nd Function
-  
-  let production = () =>{
-      setTimeout(()=>{
-          console.log("Production has started!")
-        setTimeout(()=>{
+    setTimeout(function () {
+
+        console.log(`${stocks.Fruits[fruit_name]} was selected`)
+
+        // Order placed. Call production to start
+        call_production();
+
+    }, 2000)
+};
+
+// 2nd Function
+
+let production = () => {
+    setTimeout(() => {
+        console.log("Production has started!")
+        setTimeout(() => {
             console.log("The fruit has been chopped")
-                setTimeout(()=>{
-                    console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} were added`)
+            setTimeout(() => {
+                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} were added`)
+                setTimeout(() => {
+                    console.log("Machine was started!")
+                    setTimeout(() => {
+                        console.log(`${stocks.holder[0]} is selected`)
+                        setTimeout(() => {
+                            console.log(`${stocks.toppings[0]} is selected!`)
+                                setTimeout(()=>{
+                                    console.log("The icecream was served")
+                                }, 2000)
+                        }, 3000)
+                    }, 2000)
                 }, 1000)
+            }, 1000)
         }, 2000)
-      },0000)
+    }, 0000)
     // blank for now
-  };
-  
-  // Trigger 👇
-  order(0, production);
+};
+
+// Trigger 👇
+order(0, production);
