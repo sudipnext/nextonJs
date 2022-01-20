bbde457e39ebb4322b5f5e04d384b79c2165ddd9
sudipnext <parajulisudip123@outlook.com>
2022-01-20 06:41:07 +0545

    synchronous system

---
+++ Async/async&sync.js
@@ -0,0 +1,13 @@
+// //synchronous
+
+// console.log("I am a music man")
+
+// console.log("I can play the piano")
+
+// console.log("pi pi piano")
+
+// console.log("Hello")
+
+// console.log("boss! are you listening?")
+
+//asynchronous
