01d2dc62adfd9169de118643e08c85ad0702d415
sudipnext <parajulisudip123@outlook.com>
2022-01-21 08:41:08 +0545

    cleaning things up

---
+++ 1-intro.js
@@ -0,0 +1,14 @@
+//globals - no window!!
+
+
+//__dirname - path to current directory
+//__filename - file name
+// require - function to use modules(commonJS)]
+//module - infor about current module(file)
+//process -info about env wherer the program is being executed
+// console.log(__dirname)
+setInterval(()=>{
+    console.log("hello world");
+}, 1000)
+// console.log(__filename)
+// console.log(require)
