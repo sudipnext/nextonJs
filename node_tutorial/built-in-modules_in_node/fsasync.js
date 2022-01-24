59ac169231ffd4e641121f30bf75e59346374d5e
sudipnext <parajulisudip123@outlook.com>
2022-01-24 20:39:10 +0545

    fsync and callback hell

---
+++ node_tutorial/built-in-modules_in_node/fsasync.js
@@ -0,0 +1,31 @@
+const { readFile, writeFile } = require("fs");
+const { resourceUsage } = require("process");
+// const fs = requier('fs') --2nd way to do same thing
+
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile("./content/second.txt", "utf-8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+
+    writeFile(
+      "./content/result-async.txt",
+      
+      `Here is ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result);
+      }
+    );
+  });
+});
