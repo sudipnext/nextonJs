73d7f8302930c1181aaf926f025ab9ce7309efa9
sudipnext <parajulisudip123@outlook.com>
2022-01-24 20:21:29 +0545

    flags in files

--- node_tutorial/built-in-modules_in_node/fs.js
+++ node_tutorial/built-in-modules_in_node/fs.js
@@ -5,4 +5,4 @@ const first = readFileSync('./content/first.txt', 'utf-8')
 const second = readFileSync('./content/second.txt', 'utf-8')
 console.log(first, second)
 
-writeFileSync('./content/subfolder/result.txt', `here is the result ${first} + ${second}`)
+writeFileSync('./content/subfolder/result.txt', `here is the result ${first} + ${second}`, {flag: 'a'}) // flag a does append 
