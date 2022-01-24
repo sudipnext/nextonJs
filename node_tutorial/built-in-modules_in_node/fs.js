3853358a3e99a55d4b3d915401da1a6ce350fd2a
sudipnext <parajulisudip123@outlook.com>
2022-01-24 20:19:07 +0545

    FileSystem learning

---
+++ node_tutorial/built-in-modules_in_node/fs.js
@@ -0,0 +1,8 @@
+const {readFileSync, writeFileSync} = require('fs')
+// const fs = requier('fs') --2nd way to do same thing
+
+const first = readFileSync('./content/first.txt', 'utf-8')
+const second = readFileSync('./content/second.txt', 'utf-8')
+console.log(first, second)
+
+writeFileSync('./content/subfolder/result.txt', `here is the result ${first} + ${second}`)
