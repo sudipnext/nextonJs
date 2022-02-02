b36275f67a4b8ef448fd2b26d2bf5375c73ef552
sudipnext <parajulisudip123@outlook.com>
2022-02-02 09:47:19 +0545

    added new files

--- express_tutorial/app.js
+++ express_tutorial/app.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 let {people} = require('./data')
 
-
+//static assets
+app.use(express.static('./methods-public'))
+//parse form data
+app.use(express.urlencoded({extended: false}))
 app.get('/api/people', (req, res)=>{
   res.status(200).json({sucess:true, data:people})
+})
+
+app.post('/login', (req, res)=>{
+  console.log(req.body)
+  res.send('POST')
 })
 
 app.listen(5000, () => {
